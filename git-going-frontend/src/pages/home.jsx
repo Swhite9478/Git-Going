0860e59e194a6a6e95306d66ad212bc0ef8c730a
hacker474 <hacker474@bogusemail.k3kabs.com>
2019-12-24 21:23:01 -0700

    Frontend Dockerized - initial frontend up

---
+++ git-going-frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+         <h1>Hello World! This is from React</h1>
+        );
+    }
+}
+
+export default Home;
