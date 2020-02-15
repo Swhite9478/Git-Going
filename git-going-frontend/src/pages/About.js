2f7b8c5a4d540d4bd78077bcdcaa690ffcc88f8b
Stephen White <SteveWhite.dev@gmail.com>
2020-02-15 15:13:01 -0700

    Added Carousel

--- git-going-frontend/src/pages/About.js
+++ git-going-frontend/src/pages/About.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Layout } from '../components/layouts';
+import Carousel from '../components/layouts/AboutCarousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+  carousel: {
+    paddingTop: "1rem",
+    margin: "auto",
+    width: "50%",
+  }
+}));
 
-const about = (props) => {
+export default function About() {
+
+    const classes = useStyles();
         return (
-            <div>
-                <Layout >
-                    <h1>About</h1>
-                </Layout >
-            </div>
+          <div>
+            <Layout>
+              <div className={classes.carousel}>
+                <Carousel />
+              </div>
+            </Layout>
+          </div>
         );
 }
 
-export default about;
--- git-going-frontend/src/pages/RequestForm.js
+++ git-going-frontend/src/pages/RequestForm.js
@@ -3,7 +3,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -84,7 +83,7 @@ export default function MiningForm() {
                 />
               </Grid>
             </Grid>
-            
+
             <Button
               type="submit"
               fullWidth
