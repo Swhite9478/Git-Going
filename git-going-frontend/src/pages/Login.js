c5428e853c6658dd4fbb1e86384066da1c366497
Stephen White <SteveWhite.dev@gmail.com>
2020-03-08 18:30:15 -0700

    Login is class based now

--- git-going-frontend/src/pages/Login.js
+++ git-going-frontend/src/pages/Login.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -11,150 +10,127 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import { Layout } from "../components/layouts";
 import { constants, asPath } from '../constants/constants';
 import bcrypt from 'bcryptjs';
 import emailValidator from 'email-validator';
+import React, { Component } from 'react';
+import '../styles/login.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: "91.7vh"
-  },
-  image: {
-    backgroundImage: "url(https://source.unsplash.com/featured/?tech)",
-    backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "dark"
-        ? theme.palette.grey[900]
-        : theme.palette.grey[50],
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
+export default class SignInSide extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { errorText: '', value: props.value }
   }
-}));
 
-export default function SignInSide(props) {
-  const classes = useStyles();
-
-  const handleEncryption = (password) => {
+  handleEncryption = (password) => {
     let SALT_WORK_FACTOR = 10;
     const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
     return bcrypt.hashSync(password, salt);
   }
 
-  const isValidEmail = (emailAddress) => {
+  isValidEmail = (emailAddress) => {
     return emailValidator.validate(emailAddress);
   }
 
-  const handleFormSubmission = (formDetails) => {
+  handleFormSubmission = (formDetails) => {
     let formResultObj = {
-      isValidEmail: isValidEmail(formDetails.emailAddress),
+      isValidEmail: this.isValidEmail(formDetails.emailAddress),
       authDetails: {
         emailAddress: formDetails.emailAddress,
-        encryptedPass: handleEncryption(formDetails.password)
-        }
+        encryptedPass: this.handleEncryption(formDetails.password)
+      }
     };
     return formResultObj;
   }
 
-  const onEmailFieldChange = (event) => {
-    
+  onEmailFieldChange = (event) => {
+
   }
-  
-  return (
-    <Layout>
-      <Grid container component="main" className={classes.root}>
-        <CssBaseline />
-        <Grid item xs={false} sm={4} md={7} className={classes.image} />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <div className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <form
-              className={classes.form}
-              onSubmit={(event) => {
-                const result = handleFormSubmission({
-                  emailAddress: event.target.email.value, 
-                  password: event.target.password.value
-                });
-                alert(JSON.stringify(result));
-                // props.push(result);
-                props.history.push(asPath(constants.HOME));
-              }}
-            >
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
+
+  render() {
+    return (
+      <Layout>
+        <Grid container component="main" className="root">
+          <CssBaseline />
+          <Grid item xs={false} sm={4} md={7} className="image" />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <div className="paper">
+              <Avatar className="avatar" style={{ backgroundColor:"#3949ab"}}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5" style={{textAlign:"center"}}>
+                Sign in
+              </Typography>
+              <form
+                className="form"
+                onSubmit={(event) => {
+                  const result = this.handleFormSubmission({
+                    emailAddress: event.target.email.value,
+                    password: event.target.password.value
+                  });
+                  alert(JSON.stringify(result));
+                  // props.push(result);
+                  this.props.history.push(asPath(constants.HOME));
+                }}
               >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="/SignUp" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Box mt={5}></Box>
-            </form>
-          </div>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className="submit"
+                  style={{margin: "3rem 0rem 2rem"}}
+                >
+                  Sign In
+                </Button>
+                <div style={{ marginTop: "0rem" }}>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link href="#" variant="body2">
+                        Forgot password?
+                    </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link href="/SignUp" variant="body2">
+                        Don't have an account? Sign Up
+                    </Link>
+                    </Grid>
+                  </Grid>
+                </div>
+                <Box mt={5}></Box>
+              </form>
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
+
